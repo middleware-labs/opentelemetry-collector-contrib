619ba43eddc80aceac86aeb414756814d74f51bc
ACQ053 <keval@middleware.io>
2022-09-06 14:01:35 +0530

    process.cpu.percent

--- receiver/hostmetricsreceiver/internal/scraper/processscraper/process.go
+++ receiver/hostmetricsreceiver/internal/scraper/processscraper/process.go
@@ -87,6 +87,7 @@ type processHandle interface {
 	Cmdline() (string, error)
 	CmdlineSlice() ([]string, error)
 	Times() (*cpu.TimesStat, error)
+	CPUPercent() (float64, error)
 	MemoryInfo() (*process.MemoryInfoStat, error)
 	IOCounters() (*process.IOCountersStat, error)
 	NumThreads() (int32, error)
