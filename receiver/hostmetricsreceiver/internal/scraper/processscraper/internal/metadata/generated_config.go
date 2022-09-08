392e7aea6c8bdba257bcba97b1d527bd88f869f0
ACQ053 <keval@middleware.io>
2022-09-08 10:33:53 +0530

    new resource process.started_on added

--- receiver/hostmetricsreceiver/internal/scraper/processscraper/internal/metadata/generated_config.go
+++ receiver/hostmetricsreceiver/internal/scraper/processscraper/internal/metadata/generated_config.go
@@ -111,6 +111,7 @@ type ResourceAttributesConfig struct {
 	ProcessExecutablePath ResourceAttributeConfig `mapstructure:"process.executable.path"`
 	ProcessOwner          ResourceAttributeConfig `mapstructure:"process.owner"`
 	ProcessParentPid      ResourceAttributeConfig `mapstructure:"process.parent_pid"`
+	ProcessStartedOn	  ResourceAttributeConfig `mapstructure:"process.started_on"`
 	ProcessPid            ResourceAttributeConfig `mapstructure:"process.pid"`
 }
 
@@ -132,6 +133,9 @@ func DefaultResourceAttributesConfig() ResourceAttributesConfig {
 			Enabled: true,
 		},
 		ProcessParentPid: ResourceAttributeConfig{
+			Enabled: true,
+		},
+		ProcessStartedOn: ResourceAttributeConfig{
 			Enabled: true,
 		},
 		ProcessPid: ResourceAttributeConfig{
