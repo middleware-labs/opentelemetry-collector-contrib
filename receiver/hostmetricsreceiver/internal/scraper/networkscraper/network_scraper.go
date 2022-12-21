8808c854932bce6ba231885a2e74eb95c100aafc
Keval Bhogayata <keval@middleware.io>
2022-12-21 18:04:11 +0530

    corrections after rebasing on v0.68.0 (from v0.59.0)

--- receiver/hostmetricsreceiver/internal/scraper/networkscraper/network_scraper.go
+++ receiver/hostmetricsreceiver/internal/scraper/networkscraper/network_scraper.go
@@ -17,7 +17,7 @@ import (
 	"go.opentelemetry.io/collector/scraper/scrapererror"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/internal/filter/filterset"
-	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal"
+	// "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal/scraper/networkscraper/bcal"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal/scraper/networkscraper/internal/metadata"
 )
