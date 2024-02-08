4e8fb34273b85092a8a7991e9cc8b24c55d70d13
Keval Bhogayata <90185475+bhogayatakb@users.noreply.github.com>
2024-02-08 17:45:23 +0530

    added start_time attributes in k8s workloads (#41)

--- receiver/k8sclusterreceiver/internal/deployment/deployments.go
+++ receiver/k8sclusterreceiver/internal/deployment/deployments.go
@@ -34,6 +34,7 @@ func RecordMetrics(mb *metadata.MetricsBuilder, dep *appsv1.Deployment, ts pcomm
 	rb.SetK8sDeploymentName(dep.Name)
 	rb.SetK8sDeploymentUID(string(dep.UID))
 	rb.SetK8sNamespaceName(dep.Namespace)
+	rb.SetK8sDeploymentStartTime(dep.GetCreationTimestamp().String())
 	mb.EmitForResource(metadata.WithResource(rb.Emit()))
 }
 
