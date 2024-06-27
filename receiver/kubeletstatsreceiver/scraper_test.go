13b8536033f22cfb7b3e6e0ab23109878dabba6d
Keval Bhogayata <90185475+bhogayatakb@users.noreply.github.com>
2024-06-27 18:25:45 +0530

    Fix initiated (#81)
    Co-authored-by: Hardik Choksi <hardik.choksi@middleware.io>

--- receiver/kubeletstatsreceiver/scraper_test.go
+++ receiver/kubeletstatsreceiver/scraper_test.go
@@ -867,3 +867,7 @@ func (f *fakeRestClient) Pods() ([]byte, error) {
 	}
 	return os.ReadFile("testdata/pods.json")
 }
+
+func (f *fakeRestClient) Nodes() ([]byte, error) {
+	return []byte{}, nil
+}
