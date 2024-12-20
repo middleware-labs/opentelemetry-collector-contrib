a17ad672e3e4d1e5377aa840797bc55b945e66d1
Keval Bhogayata <90185475+bhogayatakb@users.noreply.github.com>
2024-12-20 12:12:55 +0530

    Windows build fix - New scrapers applied (#101)

--- receiver/hostmetricsreceiver/internal/scraper/processscraper/process_scraper_windows.go
+++ receiver/hostmetricsreceiver/internal/scraper/processscraper/process_scraper_windows.go
@@ -18,7 +18,7 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/hostmetricsreceiver/internal/scraper/processscraper/ucal"
 )
 
-func (s *scraper) recordCPUPercentMetric(now pcommon.Timestamp, cpuPercent float64) {
+func (s *processScraper) recordCPUPercentMetric(now pcommon.Timestamp, cpuPercent float64) {
 	s.mb.RecordProcessCPUPercentDataPoint(now, cpuPercent)
 }
 
@@ -27,7 +27,7 @@ func (s *processScraper) recordCPUTimeMetric(now pcommon.Timestamp, cpuTime *cpu
 	s.mb.RecordProcessCPUTimeDataPoint(now, cpuTime.System, metadata.AttributeStateSystem)
 }
 
-func (s *scraper) recordMemoryPercentMetric(now pcommon.Timestamp, memoryPercent float32) {
+func (s *processScraper) recordMemoryPercentMetric(now pcommon.Timestamp, memoryPercent float32) {
 	s.mb.RecordProcessMemoryPercentDataPoint(now, float64(memoryPercent))
 }
 
