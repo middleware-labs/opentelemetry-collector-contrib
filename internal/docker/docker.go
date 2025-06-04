496e772ab752724796d6207c49a48f5b5e3effc9
Keval Bhogayata <90185475+bhogayatakb@users.noreply.github.com>
2025-06-04 18:21:45 +0530

    Collecting Container Status for all Docker Containers (#112)

--- internal/docker/docker.go
+++ internal/docker/docker.go
@@ -99,7 +99,6 @@ func (dc *Client) Containers() []Container {
 func (dc *Client) LoadContainerList(ctx context.Context) error {
 	// Build initial container maps before starting loop
 	filters := dfilters.NewArgs()
-	filters.Add("status", "running")
 	options := ctypes.ListOptions{
 		Filters: filters,
 	}
@@ -310,13 +309,12 @@ func (dc *Client) persistContainer(containerJSON *ctypes.InspectResponse) {
 	}
 
 	cid := containerJSON.ID
-	if !containerJSON.State.Running || containerJSON.State.Paused {
-		dc.logger.Debug("Docker container not running.  Will not persist.", zap.String("id", cid))
-		dc.RemoveContainer(cid)
-		return
-	}
+	dc.logger.Debug("Monitoring Docker container",
+		zap.String("id", cid),
+		zap.Bool("running", containerJSON.State.Running),
+		zap.Bool("paused", containerJSON.State.Paused),
+	)
 
-	dc.logger.Debug("Monitoring Docker container", zap.String("id", cid))
 	dc.containersLock.Lock()
 	defer dc.containersLock.Unlock()
 	dc.containers[cid] = Container{
