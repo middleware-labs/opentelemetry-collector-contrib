5503006926fc3675eb05b7ac11789bf5181dffa6
naman-jain-15 <naman.jain@middleware.io>
2025-11-17 10:59:37 +0530

    removed datadog reciever

--- receiver/datadoglogreceiver/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-//go:generate mdatagen metadata.yaml
-
-package datadoglogreceiver // import "github.com/open-telemetry/opentelemetry-collector-contrib/receiver/datadoglogreceiver"
