297f49d54502df23fe5d449520a679dae96fc5b6
arjun-mw <arjun.prajapati@middleware.io>
2026-01-20 18:23:06 +0530

    fix setupResoruceBuilder

--- receiver/postgresqlreceiver/scraper.go
+++ receiver/postgresqlreceiver/scraper.go
@@ -185,7 +185,7 @@ func (p *postgreSQLScraper) scrape(ctx context.Context) (pmetric.Metrics, error)
 	p.collectQueryPerfStats(ctx, now, listClient, &errs)
 	p.collectBufferHits(ctx, now, listClient, &errs)
 
-	rb := p.setupResourceBuilder(p.mb.NewResourceBuilder(), "", "", "", "")
+	rb = p.setupResourceBuilder(p.mb.NewResourceBuilder(), "", "", "", "")
 	return p.mb.Emit(metadata.WithResource(rb.Emit())), errs.combine()
 }
 
